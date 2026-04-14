79092ca81b4cc95b46f42927ecaca9df837f7562
Roberto Cano <3525807+robercano@users.noreply.github.com>
2026-04-14 15:50:05 +0200

    feat: add Vault domain type

---
+++ sdk/sdk-common/src/common/implementation/Vault.ts
@@ -0,0 +1,34 @@
+import { SerializationService } from '../../services/SerializationService'
+import { IToken } from '../interfaces/IToken'
+import { IVault, IVaultData, __signature__ } from '../interfaces/IVault'
+import { Token } from './Token'
+
+/**
+ * Type for the parameters of Vault
+ */
+export type VaultParameters = Omit<IVaultData, ''>
+
+/**
+ * @name Vault
+ * @see IVault
+ */
+export class Vault extends Token implements IVault {
+  /** SIGNATURE */
+  readonly [__signature__] = __signature__
+
+  /** ATTRIBUTES */
+  readonly asset: IToken
+
+  /** FACTORY */
+  static createFrom(params: VaultParameters): Vault {
+    return new Vault(params)
+  }
+
+  /** SEALED CONSTRUCTOR */
+  private constructor(params: VaultParameters) {
+    super(params)
+    this.asset = Token.createFrom(params.asset)
+  }
+}
+
+SerializationService.registerClass(Vault, { identifier: 'Vault' })
