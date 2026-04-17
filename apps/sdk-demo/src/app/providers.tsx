24eae57ba69674d45aff414d4497debe88b546db
Roberto Cano <3525807+robercano@users.noreply.github.com>
2026-04-17 12:54:35 +0200

    feat: reuse SDK context

--- apps/sdk-demo/src/app/providers.tsx
+++ apps/sdk-demo/src/app/providers.tsx
@@ -2,13 +2,17 @@
 
 import { SDKProvider } from '@thesolidchain/sdk-react'
 import React from 'react'
+import { AppSDKProvider } from './AppSDKContext'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const apiURL = process.env.NEXT_PUBLIC_API_URL
   const apiKey = process.env.NEXT_PUBLIC_API_KEY
+  
   return (
     <SDKProvider apiURL={apiURL ?? ''} apiKey={apiKey ?? ''}>
-      {children}
+      <AppSDKProvider>
+        {children}
+      </AppSDKProvider>
     </SDKProvider>
   )
 }
