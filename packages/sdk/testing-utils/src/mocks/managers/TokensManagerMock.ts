6f7223079208ce3968c5f931069ab51e1c621c96
Roberto Cano <3525807+robercano@users.noreply.github.com>
2026-04-26 20:38:05 +0200

    feat: added tests

--- packages/sdk/testing-utils/src/mocks/managers/TokensManagerMock.ts
+++ packages/sdk/testing-utils/src/mocks/managers/TokensManagerMock.ts
@@ -2,17 +2,40 @@ import type { IBlockchainClientProvider } from '@thesolidchain/blockchain-client
 import type { IConfigurationProvider } from '@thesolidchain/configuration-provider-common'
 import { IContractsProvider } from '@thesolidchain/contracts-provider-common'
 import { DatabaseTokensProvider, TokensManager } from '@thesolidchain/tokens-service'
+import { IChainInfo, IAddress, IToken, Token, Address } from '@thesolidchain/sdk-common'
 
 export class TokensManagerMock extends TokensManager {
   constructor() {
     super({
       providers: [
         new DatabaseTokensProvider({
-          configProvider: {} as IConfigurationProvider,
+          configProvider: {
+            getConfigurationItem: () => 'mock-table-name',
+          } as unknown as IConfigurationProvider,
           blockchainClientProvider: {} as IBlockchainClientProvider,
           contractsProvider: {} as IContractsProvider,
         }),
       ],
+    })
+  }
+
+  override async getTokenBySymbol(params: { chainInfo: IChainInfo; symbol: string }): Promise<IToken> {
+    return Token.createFrom({
+      chainInfo: params.chainInfo,
+      address: Address.createFromEthereum({ value: '0x0000000000000000000000000000000000000000' }),
+      symbol: params.symbol,
+      name: params.symbol,
+      decimals: 18,
+    })
+  }
+
+  override async getTokenByAddress(params: { chainInfo: IChainInfo; address: IAddress }): Promise<IToken> {
+    return Token.createFrom({
+      chainInfo: params.chainInfo,
+      address: params.address,
+      symbol: 'MOCK',
+      name: 'Mock Token',
+      decimals: 18,
     })
   }
 }
