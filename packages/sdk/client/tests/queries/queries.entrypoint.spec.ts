a8a0211b4a292fce518731267db372087b6b150d
Roberto Cano <3525807+robercano@users.noreply.github.com>
2026-04-29 17:36:14 +0200

    fix(client): remove missing simulateRefinanceTest import from entrypoint

--- packages/sdk/client/tests/queries/queries.entrypoint.spec.ts
+++ packages/sdk/client/tests/queries/queries.entrypoint.spec.ts
@@ -5,13 +5,11 @@ import getTokenByName from './getTokenByName.subtest'
 import getTokenBySymbol from './getTokenBySymbol.subtest'
 import makeSDKTest from './makeSDK.subtest'
 import simulateNewOrder from './newOrder.subtest'
-import simulateRefinanceTest from './simulateRefinance.subtest'
-
 describe('SDK Client', () => {
   it('should create SDK client', makeSDKTest.bind(this))
   it('should use the getLendingPool query', getLendingPoolTest.bind(this))
   it('should use the getLendingPoolInfo query', getLendingPoolInfoTest.bind(this))
-  it('should use the simulateRefinance query', simulateRefinanceTest.bind(this))
+
   it('should use the newOrder query', simulateNewOrder.bind(this))
   it('should use the getTokenBySymbol query', getTokenBySymbol.bind(this))
   it('should use the getTokenByAddress query', getTokenByAddress.bind(this))
