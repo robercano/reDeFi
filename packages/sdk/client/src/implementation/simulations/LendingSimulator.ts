99349695e3eb881a8e669ff8ef4ab329b408e19c
robercano <robercano@gmail.com>
2026-05-14 16:45:18 +0200

    refactor(client): use tRPC endpoints for LendingSimulator (phase 4)

--- packages/sdk/client/src/implementation/simulations/LendingSimulator.ts
+++ packages/sdk/client/src/implementation/simulations/LendingSimulator.ts
@@ -4,8 +4,6 @@ import {
   ITokenAmount,
   ILendingPoolId,
   ISimulation,
-  SimulationSteps,
-  LendingSimulation,
 } from '@thesolidchain/sdk-common'
 
 export class LendingSimulator implements ILendingSimulator {
@@ -15,55 +13,13 @@ export class LendingSimulator implements ILendingSimulator {
     poolId: ILendingPoolId
     amount: ITokenAmount
   }): Promise<ISimulation> {
-    await this.rpcClient.protocols.getLendingPoolInfo.query(params.poolId)
-
-    // Build the SimulationStep
-    const steps = [
-      {
-        type: SimulationSteps.DepositBorrow,
-        input: {
-          depositAmount: { value: params.amount },
-          borrowAmount: { value: undefined },
-          poolId: params.poolId,
-        },
-        output: {
-          depositAmount: params.amount,
-        },
-      } as unknown as import('@thesolidchain/sdk-common').ISimulation['steps'][number],
-    ]
-
-    // Construct intent-based simulation output
-    return new LendingSimulation({
-      steps,
-      balanceChanges: [],
-      gasEstimations: [],
-    })
+    return await this.rpcClient.simulator.lend.simulateSupply.query(params)
   }
 
   async simulateBorrow(params: {
     poolId: ILendingPoolId
     amount: ITokenAmount
   }): Promise<ISimulation> {
-    await this.rpcClient.protocols.getLendingPoolInfo.query(params.poolId)
-
-    const steps = [
-      {
-        type: SimulationSteps.DepositBorrow,
-        input: {
-          depositAmount: { value: undefined },
-          borrowAmount: { value: params.amount },
-          poolId: params.poolId,
-        },
-        output: {
-          borrowAmount: params.amount,
-        },
-      } as unknown as import('@thesolidchain/sdk-common').ISimulation['steps'][number],
-    ]
-
-    return new LendingSimulation({
-      steps,
-      balanceChanges: [],
-      gasEstimations: [],
-    })
+    return await this.rpcClient.simulator.lend.simulateBorrow.query(params)
   }
 }
