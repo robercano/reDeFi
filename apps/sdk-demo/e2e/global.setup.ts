2eaab836b2fa6e00214011afc238e913c1d97f9e
robercano <robercano@gmail.com>
2026-05-29 06:56:37 +0200

    chore(tests): standardise e2e tests to use TenderlyFork instead of AnvilFork

--- apps/sdk-demo/e2e/global.setup.ts
+++ apps/sdk-demo/e2e/global.setup.ts
@@ -1,34 +1,30 @@
-import { createAnvil, type Anvil } from '@viem/anvil'
+import { TenderlyFork } from '@thesolidchain/tenderly-utils'
+import { ChainFamilyMap } from '@thesolidchain/sdk-common'
 import * as dotenv from 'dotenv'
 
 dotenv.config({ path: '../../.env' })
 dotenv.config({ path: '../../.env.local' })
 
-let anvil: Anvil
+let fork: TenderlyFork
 
 export default async function globalSetup() {
-  console.log('Starting local Anvil Fork for E2E tests on port 8545...')
-  const alchemyKey = process.env.ALCHEMY_ENDPOINT_API_KEY
-  const forkUrl = alchemyKey 
-    ? `https://base-mainnet.g.alchemy.com/v2/${alchemyKey}`
-    : 'https://mainnet.base.org'
+  console.log('Starting Tenderly Fork for E2E tests...')
 
-  anvil = createAnvil({
-    forkUrl,
-    port: 8545,
-    startTimeout: 60000,
+  fork = await TenderlyFork.create({
+    tenderlyApiUrl: `https://api.tenderly.co/api/v1/account/${process.env.TENDERLY_USER}/project/${process.env.TENDERLY_PROJECT}`,
+    tenderlyAccessKey: process.env.TENDERLY_ACCESS_KEY as string,
+    chainInfo: ChainFamilyMap.Base.Base,
+    atBlock: 'latest',
   })
 
-  await anvil.start()
-
-  const localRpcUrl = 'http://127.0.0.1:8545'
+  const localRpcUrl = fork.forkUrl
 
   // Expose the RPC URL for Playwright tests if they need to interact directly
   process.env.NEXT_PUBLIC_TENDERLY_RPC_URL = localRpcUrl
   process.env.E2E_SDK_FORK_URL_MAINNET = localRpcUrl
 
   return async () => {
-    console.log('Tearing down local Anvil Fork...')
-    await anvil?.stop()
+    console.log('Tearing down Tenderly Fork...')
+    await fork?.dispose()
   }
 }
